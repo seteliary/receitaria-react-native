2feca571c5446bd04a1d487897c9dbf39a443f2b
“seteliary” <leticia@dngx.com.br>
2024-09-16 12:27:33 -0300

    adicionada a seção "receitas em destaque" na página inicial

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -11,8 +11,29 @@ import { fetchRecipes } from "../api";
 
 const HomeScreen = ({ navigation }) => {
   const [recipes, setRecipes] = useState([]);
+  const [featuredRecipes, setFeaturedRecipes] = useState([]);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loadingFeatured, setLoadingFeatured] = useState(true);
+
+  useEffect(() => {
+    const loadFeaturedRecipes = async () => {
+      try {
+        const randomQuery = "random"; // Use um termo de pesquisa genérico para obter receitas aleatórias
+        const data = await fetchRecipes(randomQuery);
+        // Escolha aleatoriamente 5 receitas
+        const randomRecipes = data.hits
+          .sort(() => 0.5 - Math.random())
+          .slice(0, 5);
+        setFeaturedRecipes(randomRecipes);
+      } catch (error) {
+        console.error("Error fetching featured recipes:", error);
+      } finally {
+        setLoadingFeatured(false);
+      }
+    };
+    loadFeaturedRecipes();
+  }, []);
 
   const searchRecipes = async () => {
     setLoading(true);
@@ -39,6 +60,28 @@ const HomeScreen = ({ navigation }) => {
         title="Ver Favoritos"
         onPress={() => navigation.navigate("Favorites")}
       />
+
+      <Text style={{ fontSize: 18, fontWeight: "bold", marginVertical: 10 }}>
+        Receitas em destaque
+      </Text>
+      {loadingFeatured ? (
+        <Text>Loading featured recipes...</Text>
+      ) : (
+        <FlatList
+          data={featuredRecipes}
+          keyExtractor={(item) => item.recipe.uri}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RecipeDetail", { recipe: item.recipe })
+              }
+            >
+              <Text>{item.recipe.label}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      )}
+
       {loading && <Text>Loading...</Text>}
       <FlatList
         data={recipes}
